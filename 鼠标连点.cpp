7a949ee29d0792214fd56b90997cecb358fcd6c2
赵紫辰 <97828093+ZZCjas@users.noreply.github.com>
2022-12-26 22:14:43 +0800

    Add files via upload

--- 鼠标连点.cpp
+++ 鼠标连点.cpp
@@ -4,16 +4,16 @@
 #include <typeinfo>
 using namespace std;
 double times;
+int n;
 int main()
 {
-    cout<<"按空格键开始,ESC键退出"<<endl;
     cout<<"请输入连点时间间隔:";
     cin>>times;
     while(1)
     {
         if(typeid(times)!=typeid(double))
         {
-            cout<<"请输入连点时间间隔:";
+            cout<<"请重新输入连点时间间隔:";
             cin>>times;
         }
         else
@@ -21,17 +21,44 @@ int main()
 			break;
 		}
     }
-    while(1)
+    cout<<"模式选择:\n";
+    cout<<"1.按下空格键后自动连点,ESC键退出\n2.按下空格键时连点,ESC键退出\n";
+    cout<<"请选择:";
+    cin>>n;
+    if(n==1)
     {
-        if(GetAsyncKeyState(VK_SPACE))
-        {
-            mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
-            Sleep(times);
-            if(GetAsyncKeyState(VK_ESCAPE))
-			{
-				break;
-			}
-        }
-    }
+    	bool f=0;
+    	while(!f)
+    	{
+     	    if(GetAsyncKeyState(VK_SPACE))
+        	{
+        		while(1)
+        		{
+         			mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+            		Sleep(times);
+            		if(GetAsyncKeyState(VK_ESCAPE))
+					{
+						f=1;
+						break;
+					}
+				}
+       		}
+    	}
+	}
+	else
+	{
+		while(1)
+    	{
+     	    if(GetAsyncKeyState(VK_SPACE))
+        	{
+         		mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+            	Sleep(times);
+            	if(GetAsyncKeyState(VK_ESCAPE))
+				{
+					break;
+				}
+       		}
+    	}
+	}
     return 0;
 }
